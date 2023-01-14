366fc71a65316b6d6ece70bffc803805bafd16ca
theBrash <git@bras.de>
2023-01-14 14:01:59 +0100

    added serial reconnect and one capture on startup to fix display

--- serial_plugin.py
+++ serial_plugin.py
@@ -16,13 +16,19 @@ def pibooth_startup(app, cfg):
     """Start serial connection"""
     LOGGER.info("serial startup")
     try:
-        app.serial = serial.Serial(port='/dev/ttyUSB0',  baudrate=9600, timeout=1)
+        app.serial = serial.Serial(port='/dev/ttyUSB0',  baudrate=9600, timeout=2)
         LOGGER.info("serial connection successful")
     except:
         LOGGER.info("Failed serial connection on port /dev/ttyUSB0")
 
 @pibooth.hookimpl
-def state_wait_do( app, events):
-    lastline = app.serial.readline().decode('utf-8').strip()
-    if lastline == "0":
-        app._on_button_capture_held()
+def state_wait_do(app, events):
+    if(app.serial == None):
+        try:
+            app.serial =  serial.Serial(port='/dev/ttyUSB0',  baudrate=9600, timeout=2)
+        except:
+            LOGGER.info("Failed serial connection on port /dev/ttyUSB0")
+    else:
+        lastline = app.serial.readline().decode('utf-8').strip()
+        if lastline == "0":
+            app._on_button_capture_held()
